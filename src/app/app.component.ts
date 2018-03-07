e3746ae18664cfa5b292d6f848c0728875d7c526
Kubilay Erdogan <kublaios@gmail.com>
2018-03-07 17:33:56 +0300

    adding navigation with ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showRecipes = true;
+  showShoppingList = false;
+
+  headerMenuClicked(name) {
+    if (name === 'recipes') {
+      this.showRecipes = true;
+      this.showShoppingList = false;
+    } else if (name === 'shopping-list') {
+      this.showRecipes = false;
+      this.showShoppingList = true;
+    }
+  }
 }

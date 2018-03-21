cc5952980da77678c917fbad9b77c40119943cf8
Kubilay Erdogan <kublaios@gmail.com>
2018-03-21 18:15:01 +0300

    subscribing the item select event, loading the item to the form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,23 +1,44 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
 import {NgForm} from '@angular/forms';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('myForm') myForm: NgForm;
+  editSubscription: Subscription;
+  editMode = false;
+  activeItemIndex: number;
+  activeItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.editSubscription = this.slService.startedEditing.subscribe(
+      (index: number) => {
+        this.activeItemIndex = index;
+        this.activeItem = this.slService.getIngredient(index);
+        this.editMode = true;
+        this.myForm.setValue({
+          name: this.activeItem.name,
+          amount: this.activeItem.amount,
+        });
+      }
+    );
   }
 
   onAddItem(form: NgForm) {
     const ingr = new Ingredient(form.value.name, form.value.amount);
     this.slService.addIngredient(ingr);
+  }
+
+  ngOnDestroy() {
+    this.editSubscription.unsubscribe();
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -29,4 +29,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
     this.ingredientsChangedSubscription.unsubscribe();
   }
 
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
+  }
+
 }

7c286fccbd41e547e98262215aec8ea26243429b
Kubilay Erdogan <kublaios@gmail.com>
2018-03-23 13:58:27 +0300

    firebase setting up (apikey)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAs6TB_Thql4CojT2uHww7T6Aj4F2yArSY',
+      authDomain: 'ng-recipe-book-tr.firebaseapp.com',
+    });
+  }
+}

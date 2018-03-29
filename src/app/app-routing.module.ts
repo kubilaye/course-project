9496bf047e07a9d6daf957e533ede2a688d82bb4
Kubilay Erdogan <kublaios@gmail.com>
2018-03-29 13:55:28 +0300

    core module for core stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from './core/home/home.component';
 import {AuthGuard} from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent, },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuard] }, // canLoad is for lazily loaded modules only
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuard] },
+  // canLoad is for lazily loaded modules only
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 

88c9864288a6ff94e9b8579d09e6cea3d20046e2
Kubilay Erdogan <kublaios@gmail.com>
2018-03-29 10:40:45 +0300

    securing recipes auth by using canLoad

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,13 +1,20 @@
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanLoad, RouterStateSnapshot} from '@angular/router';
 import {Injectable} from '@angular/core';
 import {AuthService} from './auth.service';
+import {Route} from '@angular/router/src/config';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private authService: AuthService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return this.authService.isAuthenticated();
   }
+
+  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isAuthenticated();
+  }
+
 }
